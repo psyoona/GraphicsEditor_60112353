3d367f1cf24ea097d8859483d2a315763250b1e7
Seyoon <Seyoon@Seyoon-PC>
2016-11-15 22:26:03 +0900

    Lastest V0.363

--- GraphicsEditor-V0.22-Factoring/src/sycom/GMain.java
+++ GraphicsEditor-V0.22-Factoring/src/sycom/GMain.java
@@ -5,6 +5,7 @@ import frame.GMainFrame;
 public class GMain {
 	public static void main(String[] args){
 		// object name & body created & bound
+		
 		GMainFrame mainFrame = new GMainFrame();
 		mainFrame.initialize();
 		mainFrame.setVisible(true);
